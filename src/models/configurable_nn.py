981e72895874a654e2807735a39285de5e0f8cad
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-03-12 17:13:37 +0100

    feat(configurable_nn): add configurable dropouts
    Co-authored-by: Gabrielle35 <123516483+Gabrielle35@users.noreply.github.com>

--- src/models/configurable_nn.py
+++ src/models/configurable_nn.py
@@ -4,19 +4,24 @@ from torch import nn
 
 
 class ConfigurableNeuralNetwork(nn.Module):
-    def __init__(self, layers: List[int]):
+    def __init__(self, layers: List[int], dropout: float = 0.0):
         super().__init__()
+
+        self.dropout = nn.Dropout(dropout)
 
         self.layers = nn.Sequential()
         self.create_layers(layers)
 
     def forward(self, x):
         return self.layers(x)
+        
 
     def create_layers(self, layers: List[int]):
         for i in range(1, len(layers) - 1):
-            self.layers.append(nn.Linear(layers[i-1], layers[i]))
+            self.layers.append(nn.Linear(layers[i - 1], layers[i]))
             self.layers.append(nn.ReLU())
+            self.layers.append(self.dropout)
 
         self.layers.append(nn.Linear(layers[-2], layers[-1]))
         self.layers.append(nn.Sigmoid())
+        self.layers.append(self.dropout)
